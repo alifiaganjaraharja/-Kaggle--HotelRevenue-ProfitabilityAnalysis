330c6bce0d5c86ee03279e3fddbffd90835ebeb0
Alifia Ganjaraharja <121094831+alifiaganjaraharja@users.noreply.github.com>
2025-04-29 11:56:00 +0700

    Add files via upload

---
+++ queries/RoomRevenue.sql
@@ -0,0 +1,9 @@
+# Analyze the Room Revenue Contribution of this hotel every month
+SELECT 
+    Month,
+    SUM(Room_Revenue) AS Total_Room_Revenue,
+    SUM(Total_Revenue) AS Total_Revenue,
+    (SUM(Room_Revenue) / SUM(Total_Revenue)) * 100 AS Room_Revenue_Contribution
+FROM hoteldata-455008.KaggleHotel.Hotel_01
+GROUP BY Month
+ORDER BY Room_Revenue_Contribution DESC;
