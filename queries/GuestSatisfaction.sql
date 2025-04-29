9866409366061fa5432786b401c21179d56af550
Alifia Ganjaraharja <121094831+alifiaganjaraharja@users.noreply.github.com>
2025-04-29 11:59:08 +0700

    Add files via upload

---
+++ queries/GuestSatisfaction.sql
@@ -0,0 +1,12 @@
+#Find out the Guest Satisfaction for each month at this hotel
+SELECT 
+  DATE_TRUNC(Date, MONTH) AS Month,
+  AVG(Average_Review_Score) AS Avg_Review_Score,
+  SUM(Complaints) AS Total_Complaints,
+  SUM(Compliment) AS Total_Compliments
+FROM 
+hoteldata-455008.KaggleHotel.Hotel_01
+GROUP BY 
+  Month
+ORDER BY 
+  Month;
