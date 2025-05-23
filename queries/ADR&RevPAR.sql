f9914c932817a8a181ab5204110c81545cde2be6
Alifia Ganjaraharja <121094831+alifiaganjaraharja@users.noreply.github.com>
2025-05-23 10:42:28 +0700

    Update ADR&RevPAR.sql

--- queries/ADR&RevPAR.sql
+++ queries/ADR&RevPAR.sql
@@ -2,8 +2,8 @@
 SELECT 
     Season, 
     DATE_TRUNC(Date, MONTH) AS Month,  -- Truncate to the first day of the month
-    AVG(ADR) AS Avg_ADR, 
-    AVG(RevPAR) AS Avg_RevPAR
+    ROUND(AVG(ADR),2) AS Avg_ADR, 
+    ROUND(AVG(RevPAR),2) AS Avg_RevPAR
 FROM hoteldata-455008.KaggleHotel.Hotel_01
 GROUP BY Season, Month
 ORDER BY Month ASC;
