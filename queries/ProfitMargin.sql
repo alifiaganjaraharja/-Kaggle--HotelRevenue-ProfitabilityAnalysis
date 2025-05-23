69f0648d25bc9169868746f8e18caa20ac024c9e
Alifia Ganjaraharja <121094831+alifiaganjaraharja@users.noreply.github.com>
2025-05-23 10:26:05 +0700

    Update ProfitMargin.sql

--- queries/ProfitMargin.sql
+++ queries/ProfitMargin.sql
@@ -3,7 +3,7 @@ SELECT
     Month,
     SUM(Total_Revenue) AS Total_Revenue,
     SUM(Total_Costs) AS Total_Costs,
-    (SUM(Total_Revenue) - SUM(Total_Costs)) / SUM(Total_Revenue) * 100 AS Profit_Margin
+    ROUND((SUM(Total_Revenue) - SUM(Total_Costs)) / SUM(Total_Revenue) * 100,2) AS Profit_Margin
 FROM hoteldata-455008.KaggleHotel.Hotel_01
 GROUP BY Month
 ORDER BY Profit_Margin DESC;
